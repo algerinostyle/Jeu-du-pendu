18c29c8b7d660b09fad7dc07366b5e864eca6dd9
algerinostyle <algerinostyle@users.noreply.github.com>
2015-05-12 03:27:09 +0200

    Update Pendu.py

--- Pendu.py
+++ Pendu.py
@@ -1,14 +1,13 @@
+import time         #importation du module time pour le temps
 
-import time
-
-name = input("Entrer le nom de joueur:")
-print ("Salut, " + name, "amusez vous bien !")
+name = input("Entrer le nom de joueur:")        #on demande le nom au joueur
+print ("Salut, " + name, "amusez vous bien !")  #on salue le joueur 
 print ("\n")
 
-regles=open("regles.txt") #affiche les règles
-regle=regles.read()
-regles.close
-print(regle)
+regles=open("regles.txt")                       #ouvre le fichier contenant les règles
+regle=regles.read()                             #on lit le fichier contenant les règles       
+regles.close                                    #ferme le fichier contenant les règles
+print(regle)                                    #on affiche les règles 
 
 #on attend une 0.5sec
 time.sleep(0.5)
@@ -17,12 +16,12 @@ print ("Chargement...")
 time.sleep(0.5)
 
 def affichemot(mot) : #fonction qui affiche le mot et le nombre d'essai restants
-    reponse = ""    #variable de type str vide
-    lettres = []    #variable de type str vide
-    vies = 7       #nombre d'essai
-    q="pendu-7.txt"
-    g=open(q,'r')
-    h=g.read()
+    reponse = ""      #variable de type str vide
+    lettres = []      #variable de type list vide
+    vies = 7          #nombre d'essai
+    q="pendu-7.txt"   #variable str ayant le nom du fichier texte ou figure ler dessin du pendu 
+    g=open(q,'r')     #ouvre le fichier dont la variable q porte le nom
+    h=g.read()        #h est la variable qui contient le dessin du fichier dont q porte le nom
     while reponse.upper()!=mot.upper()and vies !=0 :
         #tant que la réponse de l'utilisateur(donnée en majuscules) est différente du mot à trouver et le nombre de vie est différents de 0 alors
         reponse = "" #la variable est vide 
@@ -49,42 +48,42 @@ def affichemot(mot) : #fonction qui affiche le mot et le nombre d'essai restants
         print("Les lettres déja proposées sont:",lettres)        
         print(reponse)                          #on affiche le mot 
         print('Il vous reste '+nvies+' essais\n') #on affiche le nombre d'essais restant 
-    if reponse.upper()==mot.upper()and vies !=0 : #si on arrive à afficher le mot correctectement et que le nombre d'erreur est différent de 0 
+    if reponse.upper()==mot.upper()and vies !=0 : #si le joueur à afficher le mot correctectement et que le nombre d'erreur est différent de 0 
         print("Vous avez deviné le mot","\'",mot.upper(),"\'","!") #alors on affiche que le joueur a gagné et le mot
-        gagne=open("gagne.txt","r")
-        w=gagne.read()                          
-        gagne.close()
-        print(w)
+        gagne=open("gagne.txt","r")                 #ouvre le fichier gagne.txt qui contient l'image afficher lorsque le joueur gagne
+        w=gagne.read()                              #w contient le dessin "gagnant" contenu dans gagne.txt        
+        gagne.close()                               #on ferme gagne.txt
+        print(w)                                    #on affiche le dessin gagnant
         recommencer=input("Entrer une lettre pour recommencer:") #on propose de rejouer
         if type(recommencer)==str:                               #grâce à cette boucle on peut rejouer directement
                 mot=trouvermot()
                 affichemot(mot)            
-    if reponse.upper()!=mot.upper() and vies ==0: #mais si on n'yarrive pas alors le joueur a perdu
-        print("Perdu le mot à trouver était","\'", mot.upper(),"\'")
-        recommencer=input("Entrer une lettre pour recommencer")
-        if type(recommencer)==str:
+    if reponse.upper()!=mot.upper() and vies ==0:                #mais si le joueur n'y arrive pas alors le joueur a perdu
+        print("Perdu le mot à trouver était","\'", mot.upper(),"\'") #on affiche que le joueur a perdu et le mot à trouver
+        recommencer=input("Entrer une lettre pour recommencer")   
+        if type(recommencer)==str:                              #grâce à cette boucle on peut rejouer directement
                 mot=trouvermot()
                 affichemot(mot)
             
        
 
-def trouvermot() : #
-    import random #importe le module random
-    f=open('dico.txt','r') #on ouvre le dictionnaire pour le lire       
-    line= random.randrange(0,336531) #le dico est composée de lignes qui corresponde chacun à un mot donc line est la ligne du mot choisie aléatoirement   
-    for n in range (0,line):
+def trouvermot() :                       #fonction chargée de choisir un mot aléatoirement
+    import random                        #importe le module random
+    f=open('dico.txt','r')               #on ouvre le dictionnaire pour le lire       
+    line= random.randrange(0,336531)     #le dico est composée de lignes qui corresponde chacun à un mot donc line est la ligne du mot choisie aléatoirement   
+    for n in range (0,line):             #pour n de 0 jusqu'à la ligne du mot
         mot=f.readline().replace('\n','') #lire le mot choisie     
-    f.close()# on ferme le dico
-    return mot #on retourne le mot 
+    f.close()                            # on ferme le dico
+    return mot                           #on retourne le mot 
 
-def nbvies(mot,L,vies) : #fonction paramétrique du nombre d'essais en fonction du nombre d'erreur 
-    if L.upper() in mot.upper() : #si la lettre proposé est dans le mot demandé alors le nombre d'essais reste le meme 
-        vies=vies
-    elif len(L)!=1:
-        vies=vies
+def nbvies(mot,L,vies) :                 #fonction paramétrique du nombre d'essais en fonction du nombre d'erreur 
+    if L.upper() in mot.upper() :        #si la lettre proposé est dans le mot demandé alors le nombre d'essais reste le meme 
+        vies=vies                        #le nombre de vies ne change pas 
+    elif len(L)!=1:                      #si le joueur entre plusieurs lettres
+        vies=vies                        #le nombre de vies ne change pas              
     else :
-        vies=vies-1 #sinon si on fait une erreur le nombre d'essais diminue de un
-    return vies #on retourne le nombre d'essais 
+        vies=vies-1                      #sinon si on fait une erreur le nombre d'essais diminue de 1
+    return vies                          #on retourne le nombre d'essais 
 
 
 
